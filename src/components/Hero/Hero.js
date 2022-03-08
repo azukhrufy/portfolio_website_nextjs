287dc5be3ed915ae84eb666cb446864db567e820
azukhrufy <azukhrufy@outlook.com>
2022-03-08 16:28:37 +0700

    adding the hero, content hero, and applied some styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        Azukhrufy Portfolio
+      </SectionTitle>
+      <SectionText>
+      Currently as a software engineer at Neural Technologies Indonesia, responsible to develop ERP System and Collaborates with all of element for implementing advance business solution.
+      Experience in Mobile App development, Web Front End Development and leading an organization. Passionate to learn something new about technology.
+      </SectionText>
+      <Button onClick={() => window.location='https://www.linkedin.com/in/azukhrufy/'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
