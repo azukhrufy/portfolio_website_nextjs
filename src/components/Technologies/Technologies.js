55221c944f583015385a350aec106b4ff990e7d4
azukhrufy <anandazukhrufawalwi@gmail.com>
2022-03-11 14:00:04 +0700

    develop the whole Technologies comp consept

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiZend, DiSwift, DiPython, DiAndroid, DiGit } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <SectionTitle main>Technologies</SectionTitle>
+    <SectionText>
+
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiSwift size="3rem"/>
+        <ListContainer>
+          <ListTitle>iOS Swift</ListTitle>
+          <ListParagraph>
+            Experience in develop <br />
+            iOS applications with Swift
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>Front - End</ListTitle>
+          <ListParagraph>
+            Experience in develop <br/>
+            React.js and React Native
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPython size="3rem"/>
+        <ListContainer>
+          <ListTitle>Python</ListTitle>
+          <ListParagraph>
+            Experience in develop <br/>
+            Odoo using Python
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
