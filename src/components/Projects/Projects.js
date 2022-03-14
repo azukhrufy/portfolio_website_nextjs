8c439537fe38c5f077d7f24be5f6617a074b2a1b
azukhrufy <anandazukhrufawalwi@gmail.com>
2022-03-14 17:33:00 +0700

    add section of personal project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import { projects,personalProjects } from '../../constants/constants';
 
 const Projects = () => (
   <Section nopadding id='projects'>
@@ -31,8 +31,36 @@ const Projects = () => (
               ))}
             </TagList>
           </div>
+        </BlogCard>
+      ))}
+    </GridContainer>
+    <SectionDivider />
+    <SectionTitle main>Personal Projects</SectionTitle>
+    <GridContainer>
+      {personalProjects.map(({id, image, title, description, tags, source, visit}) =>(
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent >
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>
+            {description}
+          </CardInfo>
+          <br/>
+          <div>
+            <TitleContent>
+              <HeaderThree size="3rem">Stack</HeaderThree>
+            </TitleContent>
+            <Hr/>
+            <TagList>
+              {tags.map((tag, i) => (
+                <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
           <UtilityList>
-            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={visit}>Visit</ExternalLinks>
             <ExternalLinks href={source}>Source</ExternalLinks>
           </UtilityList>
         </BlogCard>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -37,6 +37,45 @@ export const projects = [
   },
 ];
 
+export const personalProjects = [
+  {
+    title: 'Hackernews Clone',
+    description: "Hackernews Clone is my personal project to implemented the Hackernews API in my React JS Project",
+      image: '/images/hackernews.png',
+      tags: ['React', 'Hackernews API'],
+    source: 'https://github.com/azukhrufy/react-hackernews',
+    visit: 'https://hackernews.azukhrufy.vercel.app/',
+    id: 0,
+  },
+  {
+    title: 'digiSchool',
+    description:"digiSchool is a website-based application intended for al izhar schools, made with Nuxt JS, this application contains attendance, and eLearning for students.",
+    image: '/images/digischool.png',
+    tags: ['Nuxt Js', 'LoopBack 4', 'Node Js'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  {
+    title: 'eBos',
+    description: "eBos is a website for administration and processing of subsidized funds for elementary schools, this application was made using Next JS and Redux Saga",
+      image: '/images/ebos.png',
+      tags: ['Next Js', 'Redux Saga'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 2,
+  },
+  {
+    title: 'Smartfren Kiosk Dashboard',
+    description: "Kiosk Dashboard is a website for managing smartfren kiosk vending machines, like upgrading the firmware, activating and deactivating vending machines. Built with Angular JS and Loopback 3",
+    image: '/images/smartfren.png',
+    tags: ['Angular Js', 'Loopback 3', 'Node Js'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 3,
+  },
+];
+
 export const TimeLineData = [
   { year: 2017, text: 'Started my journey as Software Developer, joined Politeknik Negeri Bandung', },
   { year: 2019, text: 'Started my Internship as Mobile Developer, Created Musfiratur App with using React Native Framework', },
