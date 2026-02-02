03a371080bc027a17e322a1b64922dcc05c82bcc
azukhrufy <anandazukhrufawalwi@gmail.com>
2026-02-02 11:48:50 +0700

    fix: change align to center

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -10,6 +10,8 @@ export const Section = styled.section`
   position: relative;
   overflow: hidden;
   grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  align-items: center;
 
   @media ${(props) => props.theme.breakpoints.md} {
     padding: 24px 48px 0;
